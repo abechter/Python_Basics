fb4f53447ef467d9cb96f816048d6ae30996cd71
Andreas Bechter <andreas.bechter@M032383PBDV7N.community.veritas.com>
2016-12-30 18:42:17 +0100

    add loop to rename files in directory

--- Project2/rename_files.py
+++ Project2/rename_files.py
@@ -3,8 +3,16 @@ import os
 def rename_files():
 	#(1) get file names from a folder
 	file_list = os.listdir("/Users/andreas.bechter/Udacity/Python_Basics/Project2/prank")
-	print(file_list)
+	#print(file_list)
+	saved_path = os.getcwd()
+	print("Current working directory is" +saved_path)
+	os.chdir("/Users/andreas.bechter/Udacity/Python_Basics/Project2/prank")
 
 	#(2) for each file, rename filename
+	for file_name in file_list:
+		print("Old Name - "+file_name)
+		print("New Name -"+file_name.translate(None,"0123456789"))
+		os.rename(file_name, file_name.translate(None,"0123456789"))
+	os.chdir(saved_path)
 
 rename_files()
