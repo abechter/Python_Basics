16e4e23b372e68630d770be8b67baf2e507c47ba
Andreas Bechter <andreas.bechter@M032383PBDV7N.community.veritas.com>
2017-01-01 15:19:12 +0100

    improve code quality

--- Project3/mindstorms.py
+++ Project3/mindstorms.py
@@ -1,30 +1,34 @@
 import turtle
 
 def draw_square():
-	window = turtle.Screen()
-	window.bgcolor("red")
-
 	brad = turtle.Turtle()
 
 	brad.color("green")
 	brad.shape("turtle")
 	brad.speed(3)
 
-	
-	brad.forward(100)
-	brad.right(90)
-	brad.forward(100)
-	brad.right(90)
-	brad.forward(100)
-	brad.right(90)
-	brad.forward(100)
-	brad.right(90)
+	i = 0
+	while (i < 4): 
+		brad.forward(100)
+		brad.right(90)
+		i = i + 1
 
+def draw_circle():
 	angie = turtle.Turtle()
+	
 	angie.color("blue")
 	angie.shape("arrow")
 	angie.circle(50)
 
-	window.exitonclick()
 
-draw_square()
+def main():
+        window = turtle.Screen()
+        window.bgcolor("red")
+
+        draw_square()
+        draw_circle()
+
+        window.exitonclick()
+
+
+main()
